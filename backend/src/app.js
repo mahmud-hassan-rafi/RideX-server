416a1d8019fdcafd9da251c786d439d1c1342902
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2025-12-02 14:24:59 +0600

    modified: module path

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectToDB from "@config/db.js";
+import connectToDB from "./config/db.js";
 dotenv.config();
 
 const app = express();
